59f6294361d51492c7fab263744b01a582d39649
Francesca Bagordo <bagordofrancesca587@gmail.com>
2023-11-04 00:18:33 +0100

    inserite card per homePage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {getListaIphone} from "../service/IphoneService";
+import { getListaIphone } from "../service/IphoneService";
 
 function Home() {
     const [listaIphone, setListaIphone] = useState([])
@@ -10,10 +10,24 @@ function Home() {
         }).catch(error => {
             alert(error)
         })
-      },[]);
+    }, []);
 
     return (
-       <div>HOME</div>
+        <div className="container">
+            {
+                listaIphone.length > 0 ? listaIphone.map((iphone, index) => (
+                    <div className="card" style={{ width: "18rem" }} key={index}>
+                      <img src="..." className="card-img-top" alt="..." />
+                      <div className="card-body">
+                        <h5 className="card-title">{iphone.model}</h5>
+                        <p className="card-text">{iphone.description}</p>
+                        <a href="#" className="btn btn-primary">Dettagli</a>
+                      </div>
+                    </div>
+                  )) : null
+            }
+        </div>
+
     )
 }
 export default Home
